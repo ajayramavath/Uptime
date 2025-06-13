7e5bcf26f449090ed93d5364262d76abf3536720
ajayramavath <ajayramavath03@gmail.com>
2025-06-14 05:25:15 +0530

    updated schema

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,26 +1,10 @@
 import express from "express";
 import cors from "cors";
-
 import { client } from "@uptime/db"
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-function initDb() {
-  client.$connect().then(() => {
-    console.log("Connected to db")
-  })
-}
 
-initDb()
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-})
-
-app.get("/users", async (req, res) => {
-  const users = await client.user.findMany()
-  res.status(200).json({ data: users })
-})
-
-app.listen(8080);
+app.listen(8080); 
